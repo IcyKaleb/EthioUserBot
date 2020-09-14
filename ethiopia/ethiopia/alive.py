f1d12dd2e05cc7029c7b7579c975bb812de88c8e
ヒューニマ <xalebg@gmail.com>
2020-09-14 07:52:12 +0300

    ethiopia

--- ethiopia/ethiopia/alive.py
+++ ethiopia/ethiopia/alive.py
@@ -3,7 +3,7 @@ import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 from platform import uname
-from ethiopia import ALIVE_NAME
+from ethiopia import ETHIO_NAME
 from ethiopia.utils import admin_cmd
 
 DEFAULTUSER = str(ETHIO_NAME) if ETHIO_NAME else "No Name set yet, please add ETHIO_NAME."
