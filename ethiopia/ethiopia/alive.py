164af036d44c02079b76fc81bb7d2b4d8b1eedf3
Kaleb <xalebg@gmail.com>
2020-10-09 13:33:53 +0300

    Prefix Changed

--- ethiopia/ethiopia/alive.py
+++ ethiopia/ethiopia/alive.py
@@ -8,7 +8,7 @@ from ethiopia.utils import admin_cmd
 
 DEFAULTUSER = str(ETHIO_NAME) if ETHIO_NAME else "No Name set yet, please add ETHIO_NAME."
 
-@command(outgoing=True, pattern="^.alive$")
+@command(outgoing=True, pattern="^*alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("EthioUserBot is running.\n\n"
