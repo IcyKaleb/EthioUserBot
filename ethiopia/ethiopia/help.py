ae15649b878d6ed7713826c7349b6c94ff7a39a0
Kaleb <70502151+Kalebpy@users.noreply.github.com>
2020-10-14 14:40:59 +0300

    Update help.py

--- ethiopia/ethiopia/help.py
+++ ethiopia/ethiopia/help.py
@@ -1,6 +1,14 @@
-from ethiopia import CMD_LIST
+# Ported from other Telegram UserBots for TeleBot
+# Kangers, don't remove this line 
+# @its_xditya
 
-@command(pattern="^.help ?(.*)")
+from ethiopia import CMD_LIST
+from ethiopia import ALIVE_NAME
+from ethiopia.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TeleBot User"
+
+@telebot.on(admin_cmd(pattern="help ?(.*)"))
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
@@ -8,27 +16,37 @@ async def cmd_list(event):
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "ℹ️ " + i + "\n"
+                string += "⚡ " + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
                 string += "\n"
             if len(string) > 4095:
-                await borg.send_message(event.chat_id, "Do .help cmd")
-                await asyncio.sleep(5)
+                with io.BytesIO(str.encode(string)) as out_file:
+                    out_file.name = "cmd.txt"
+                    await tgbot.send_file(
+                        event.sender_id,
+                        out_file,
+                        force_document=True,
+                        allow_cache=False,
+                        caption="**COMMANDS**",
+                        reply_to=reply_to_id
+                    )
+                    await event.delete()
             else:
                 await event.edit(string)
         elif input_str:
             if input_str in CMD_LIST:
-                string = "Commands found in {}:\n".format(input_str)
+                string = "**Commands available in {}** \n\n".format(input_str)
                 for i in CMD_LIST[input_str]:
                     string += "    " + i
                     string += "\n"
+                string +="\n**© @TeleBotSupport**"
                 await event.edit(string)
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """EthioUserBot Helper Made by @EthioUserBot"""
+            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[TeleBot](https://xditya.gitbook.io/telebot/)**\n\n"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
