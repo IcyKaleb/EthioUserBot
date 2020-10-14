234eee306494e6438a1c9d1b724ac34dd4804cb7
Kaleb <70502151+Kalebpy@users.noreply.github.com>
2020-10-14 14:24:43 +0300

    Update antivirus2.py

--- ethiopia/ethiopia/antivirus2.py
+++ ethiopia/ethiopia/antivirus2.py
@@ -21,7 +21,7 @@ async def _(event):
     if reply_message.sender.bot:
        await event.edit("```Reply to actual users message.```")
        return
-    await event.edit(" `Sliding my tip, of fingers over it`")
+    await event.edit("**Scanning File**")
     async with borg.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=161163358))
