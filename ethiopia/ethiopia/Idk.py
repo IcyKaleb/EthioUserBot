245e81e8f5acb08cd0e3d59266c699f92d52acaf
Kaleb <70502151+Kalebpy@users.noreply.github.com>
2020-10-14 17:33:29 +0300

    Update Idk.py

--- ethiopia/ethiopia/Idk.py
+++ ethiopia/ethiopia/Idk.py
@@ -8,7 +8,7 @@ from ethiopia.utils import admin_cmd
 
 DEFAULTUSER = str(ETHIO_NAME) if ETHIO_NAME else "TeleBot User"
 
-@telebot.on(admin_cmd(pattern="help ?(.*)"))
+@borg.on(admin_cmd(pattern="help ?(.*)"))
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
