ecbe36c8c9eedf395b8df9565b62e0d83596a33a
Kaleb <70502151+Kalebpy@users.noreply.github.com>
2020-10-15 09:33:27 +0300

    Update pmpermit_menu.py

--- ethiopia/ethiopia/pmpermit_menu.py
+++ ethiopia/ethiopia/pmpermit_menu.py
@@ -23,7 +23,7 @@ import asyncio
 import io 
 import telethon.sync
 from telethon.tl.functions.users import GetFullUserRequest
-import ethiopia.plugins.sql_helper.pmpermit_sql as pmpermit_sql
+import ethiopia.ethiopia.sql_helper.pmpermit_sql as pmpermit_sql
 from telethon import events, errors, functions, types
 from ethiopia import ETHIO_NAME, LESS_SPAMMY
 from ethiopi.utils import admin_cmd
