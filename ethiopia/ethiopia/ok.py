b43ca16b530c6c4b1c136dbb31e966153c291a25
Girum <70520544+girume1@users.noreply.github.com>
2020-10-15 10:46:54 +0300

    update ok.py

--- ethiopia/ethiopia/ok.py
+++ ethiopia/ethiopia/ok.py
@@ -10,37 +10,37 @@ import asyncio
 
 from ethiopia.utils import admin_cmd
 
-@borg.on(admin_cmd("(.*)"))
+@borg.on(admin_cmd("ok"))
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.00001
-    animation_ttl = range(0, 90)
-    input_str = event.pattern_match.group(1)
-    if input_str == "ok":
-        await event.edit(input_str)
-        animation_chars = [
-            "F",
-            "U",
-            "C",
-            "K",
-            "Y",
-            "O",
-            "U",
-            "B",
-            "C",
-            "FK",
-            "UU",
+    animation_interval = 0.1
+    animation_ttl = range(0,36)
+    #input_str = event.pattern_match.group(1)
+   # if input_str == "ok":
+    await event.edit("ok")
+    animation_chars = [
+            "OK",
+            "BOSS",
+            "OK MAN",
+            "OK BITCH",
+            "OK FUKCER",
+            "OK SEXY BABE",
+            "OK GAY",
+            "OK SIR",
+            "GO AND SAY OK",
+            "OK LOL",
+            "YAA OK",
             "FCUK",
-            "UOY",
-            "C",
-            "F",
-            "Y",
-            "F",
-            "Ok Sar 😇"
+            "OK",
+            "Boss",
+            "Yeahhhhhh",
+            "O",
+            "K",
+            "Ok Boss! 😇"
         ]
 
-        for i in animation_ttl:
+    for i in animation_ttl:
         	
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 18])
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 18])
