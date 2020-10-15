04aa7535a9978698f3fd8d905d3f431c1438464b
Girum <70520544+girume1@users.noreply.github.com>
2020-10-15 17:23:36 +0300

    update pmpermit.py

--- ethiopia/ethiopia/pmpermit.py
+++ ethiopia/ethiopia/pmpermit.py
@@ -2,7 +2,7 @@ import os
 import time
 import asyncio
 import io
-import ethiopia.plugins.sql_helper.pmpermit_sql as pmpermit_sql
+import ethiopia.ethiopia.sql_helper.pmpermit_sql as pmpermit_sql
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon import events, errors, functions, types
 from ethiopia import ETHIO_NAME, CUSTOM_PMPERMIT
